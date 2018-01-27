8750f64d871a9bdc444d88e49214748fde205f9f
Afaan Naqvi <Afaan.Naqvi@katerra.com>
2018-01-27 15:37:59 -0700

    Add verification for user zipcode input

--- station_zipcode_mapping.py
+++ station_zipcode_mapping.py
@@ -14,8 +14,6 @@ mapped_stations = pd.read_csv(mapped_stations_file, header = None, names = csv_c
 # Read in excel file as a df. Convert zip codes to strings so leading zeros are not dropped
 mapped_zipcodes = pd.read_csv(mapped_zipcodes_file, converters={'Zip':str})
 
-# Get zipcode from user
-user_zip_input = input("Enter the Zip Code: ")
 
 # Function that takes user entered zip code and returns the zip code centroid lat-lon
 def get_zip_centroid(zip_df, zip_code=int):
@@ -49,12 +47,23 @@ def get_min_dist(df_mapped_stations):
 	min_dist_station = df_mapped_stations.loc[df_mapped_stations['Dist_to_Centroid'] == min_dist]
 	return min_dist_station
 
-# Call the get_zip_centroid function on the mapped_zipcodes and the user entered zip code
-zip_centroid_tuple = get_zip_centroid(mapped_zipcodes, user_zip_input)
+# Loop to call all other functions
+# Use try except loop for verification
+while True:
+	try:
+		# Get zipcode from user
+		user_zip_input = input("Enter the Zip Code: ")
+		# Call the get_zip_centroid function on the mapped_zipcodes and the user entered zip code
+		zip_centroid_tuple = get_zip_centroid(mapped_zipcodes, user_zip_input)
+	except:
+		print('Invalid zip-code. Please try again.')
+		continue
+	else:
+		# Call the apply_calc_dist function
+		apply_calc_dist(mapped_stations, zip_centroid_tuple)
+		# Call the get_min_dist function to return the closest station to the user entered zip file
+		return_station = get_min_dist(mapped_stations)
+		print("The closest weather station is: ", return_station.loc[:, :'Lon'])
+		break
 
-# Call the apply_calc_dist function
-apply_calc_dist(mapped_stations, zip_centroid_tuple)
 
-# Call the get_min_dist function to return the closest station to the user entered zip file
-return_station = get_min_dist(mapped_stations)
-print("The closest weather station is: ", return_station.loc[:, :'Lon'])
